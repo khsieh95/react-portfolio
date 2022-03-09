306346af761efbe64f032d7ac92209773a26029a
Kevin Hsieh <khsieh95@gmail.com>
2022-03-09 13:23:19 -0500

    update resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import "./style.css";
-<<<<<<< HEAD
 import resume from "../files/Kevin-Hsieh-Tech-Resume.pdf";
-=======
-import resume from "../files/Khsieh.pdf";
->>>>>>> 0eb26c1375d35ebc51d48fbb76515608ac74cd48
 
 function Resume() {
   return (
